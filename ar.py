cf42e6be9e51f1b42cbc8ccb2f50337cde6ea076
chalakoppashraddha-debug <chalakoppashraddha@gmail.com>
2026-01-02 20:05:45 +0530

    Create ar.py

---
+++ ar.py
@@ -0,0 +1,12 @@
+def main():
+    scores = [78, 85, 90, 66, 88]
+
+    total = sum(scores)
+    average = total / len(scores)
+
+    print("== main/master branch output ==")
+    print(f"scores: {scores}")
+    print(f"sum: {total}")
+    print(f"average: {average}")
+  if __name__ == "__main__":
+    main()
